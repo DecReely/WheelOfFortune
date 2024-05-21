1bc21f9d476c98add2c7eabe945e681205962b6c
Emir Kaan Öz <emirkaanoz261@gmail.com>
2024-05-21 14:10:41 +0300

    Added Enums

---
+++ Assets/[GAME]/Scripts/Core/Enums.cs
@@ -0,0 +1,69 @@
+namespace CriticalStrike.WheelOfFortune.Core
+{
+    public static class Enums
+    {
+
+        public enum ZoneType
+        {
+            Normal,
+            Safe,
+            Super
+        }
+        
+        public enum ItemType
+        {
+            Bomb = 0,
+            
+            Gold = 1,
+            Cash = 2,
+            
+            Molotov = 10,
+            GrenadeM26 = 21,
+            GrenadeM67 = 22,
+            Healthshot2Neurostim = 40,
+            Healthshot2Regenerator = 41,
+            
+            ChestBronze = 100,
+            ChestSilver = 101,
+            ChestGold = 102,
+            ChestSmall = 120,
+            ChestStandard = 121,
+            ChestBig = 122,
+            ChestSuper = 123,
+            
+            ArmorPoint = 200,
+            KnifePoint = 201,
+            PistolPoint = 202,
+            RiflePoint = 203,
+            ShotgunPoint = 204,
+            SMGPoint = 205,
+            SniperPoint = 206,
+            SubmachinePoint = 207,
+            VestPoint = 208,
+            
+            Tier1Shotgun = 500,
+            Tier2Shotgun = 501,
+            Tier3Shotgun = 502,
+            Tier1Melee = 503,
+            Tier2Melee = 504,
+            Tier3Melee = 505,
+            Tier1Rifle = 506,
+            Tier2Rifle = 507,
+            Tier3Rifle = 508,
+            Tier1SMG = 509,
+            Tier2SMG = 510,
+            Tier3SMG = 511,
+            Tier1Sniper = 512,
+            Tier2Sniper = 513,
+            Tier3Sniper = 514,
+            
+            HelmetPumpkin = 1000,
+            
+            MeleeBayonetEaster = 10000,
+            AviatorGlassesEaster = 10001,
+            BaseballCapEaster = 10002,
+            
+            MeleeBayonetSummerVice = 12000
+        }
+    }
+}
